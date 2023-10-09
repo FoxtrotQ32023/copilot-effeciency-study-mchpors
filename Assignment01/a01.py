b34dcd6bf35d9fc9d70b0881aa42372bae7a588c
Marie Pors <mariechpors@gmail.com>
2023-10-09 17:10:25 +0200

    package installed and assignment continued

--- Assignment01/a01.py
+++ Assignment01/a01.py
@@ -15,36 +15,29 @@ def read_data(filepath):
         
         all_coordinates_str = lines[1+count:1+count+n]
         #print("all_coordinates_str:\n", all_coordinates_str)
-        latitudes = []
-        longitudes = []
+        coordinates = []
 
-        for c in all_coordinates_str:
-            #print("c:", c)
-            coordinate = c.split()
-            #print("coordinate:", coordinate)
-            latitudes.append(float(coordinate[0]))
-            longitudes.append(float(coordinate[1]))
+        for coord_str in all_coordinates_str:
+            coord_list = coord_str.split()
+            coordinates.append((float(coord_list[0]),float(coord_list[1])))
 
-        #print("lat:", latitudes)
-        #print("lon:", longitudes)
-
-        sets.append([n,latitudes,longitudes])
+        sets.append([n,coordinates])
 
         count = count + n + 1
 
-    #("sets: ", sets)
     return sets
 
 sets = read_data('Assignment01/airlinehub2.in')
+print("sets: ", sets)
 
 set_ = sets[0]
-set_lat = set_[1]
-set_lon = set_[2] 
-location1 = (set_lat[0],set_lon[0])
-location2 = (set_lat[1],set_lon[1])
-print(location1)
-print(location2)
+set_coords = set_[1]
+location1 = set_coords[0]
+location2 = set_coords[0]
+#print(location1)
+#print(location2)
 
 # https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
 import geopy.distance
 dist = geopy.distance.geodesic(location1, location2).km
+print("dist = ", dist)
