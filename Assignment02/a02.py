6b46539c9329d2851126fb2ca4420ba21b5f87be
Marie Pors <mariechpors@gmail.com>
2023-10-12 16:42:09 +0200

    Start assignment using Copilot

---
+++ Assignment02/a02.py
@@ -0,0 +1,13 @@
+# **String Multimatching**
+#Example input:
+#      2          <-- Number of search terms
+#      p          <-- First search term
+#      pup        <-- Second search term
+#      popup      <-- Line to search in
+
+#For each test case, output n lines, where the _i_’th line contains the positions of all the occurrences of the _i_’th pattern in text, from first to last, separated by a single space.
+
+#Example output (0 based array):
+#      2 4        <-- Index positions for the first search term "p", separated with space
+#      2          <-- Index position for the second search term "pup"
+#The problem is to find the index positions of the search terms in the input text and print a line for each search term.
